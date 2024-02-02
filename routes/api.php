a1d8d8d13d8b47d789cee770f86ded28e086c04c
Rener Pires <rener.gbrl.p@gmail.com>
2024-02-01 22:55:02 -0300

    test(routes): testing heath check
    create integration tests for health check route at root level

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', static function () {
-    return response()->json(["service"=>"user-management-service", "status"=>"ok", "version"=>"1.0.0"], 200);
+    return response()->json([
+        "service"=>"user-management-service",
+        "status"=>"ok",
+        "version"=>"1.0.0"
+    ], 200);
 });
-
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
--- tests/Feature/HealthCheckTest.php
+++ tests/Feature/HealthCheckTest.php
@@ -5,15 +5,20 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class HealthCheckTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_root_path_health_check(): void
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+
+        $response->assertJson([
+            "service"=>"user-management-service",
+            "status"=>"ok"
+        ]);
     }
 }
