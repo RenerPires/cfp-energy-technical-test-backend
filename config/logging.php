e8f54f2469f5a48b451510863c9a541527e1b081
Rener Pires <rener.gbrl.p@gmail.com>
2024-02-08 18:05:27 -0300

    feat(support): add cors support for frontend integration

--- config/logging.php
+++ config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'daily', 'syslog', /*'slack'*/],
+            'channels' => ['single', 'daily', 'syslog','slack'],
             'ignore_exceptions' => false,
         ],
 
